da7e795eff5143ef33f44ed89e75825dc8f4073f
Riley <bigelow.riley.j@gmail.com>
2020-11-10 13:41:46 -0700

    setting up assingment structure and files

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+//var mathmodel=require('models/mathmodel');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//where static files live
+app.use(express.static(__dirname + '/public'));
+
+// view directory for all files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+// rule requesting "/math" should be handled by the
+// handleMath in model js 
+app.get('/rates', handleRates);
+
+// start the server listening
+app.listen(port, function() {
+  console.log('Node app is running on port', port);
+});
+
+function handleRates(request, response) {
+	/*const operation = request.query.operation;
+	const operand1 = Number(request.query.operand1);
+	const operand2 = Number(request.query.operand2);
+	calculateRate(response, operation, operand1, operand2);*/
+}
+
+
+function calculateRate(response, type, weight) {
+	op = op.toLowerCase();
+
+
+	/*// JSON object of the values  to pass  to the EJS result
+	const params = {operation: op, left: left, right: right, result: result};
+
+	// Render the response, using "result.ejs" in the pages directory
+	response.render('pages/result', params);*/
+}
+
