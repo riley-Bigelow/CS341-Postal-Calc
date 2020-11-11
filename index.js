aab839c499a2cd7265fbb8e9716fcb8b44f558f4
Riley <bigelow.riley.j@gmail.com>
2020-11-11 13:26:03 -0700

    test

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function calculateRate(response, weight, type, zone) {
   cost = cost.toFixed(2);
   console.log(cost);
   
-  if(zone == "NAN"){
+  if(zone == NaN){
     zone = "Not Applicable";
   }
 
