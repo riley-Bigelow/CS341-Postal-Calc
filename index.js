796b153d56d9f342c2425acfa8f22f32500c613b
Riley <bigelow.riley.j@gmail.com>
2020-11-10 20:03:22 -0700

    calculateRate() logic beign added

--- index.js
+++ index.js
@@ -21,19 +21,32 @@ app.listen(port, function() {
 
 function handleRates(request, response) {
 	const weight = parseFloat(request.query.weight);
-	const itemtype = request.query.itemtype;
-	calculateRate(response, weight,itemtype);
+  const itemtype = request.query.itemtype;
+  const quant= Number(request.query.itemtype);
+	calculateRate(response, weight,itemtype,quant);
 }
 
 
-function calculateRate(response, type, weight) {
-
-  console.log(weight + " " + type);
+function calculateRate(response, type, weight, quant) {
+  var cost = 0.00
+  console(weight + " " + type);
+  if(type == 'Letters(Stamped)'){
+    cost = stampedLetters(weight, quant);
+  }
+  else if(type == "Letters(Metered)"){
+    cost = meteredLetters(weight, quant);
+  }
+  else if(type="Large Envelopes(Flats)"){
+    cost = flats(weight, quant);
+  }
+  else{
+    cost = packages(weight,quant);
+  }
 
 	// JSON object of the values  to pass  to the EJS result
-	const params = {weight: weight, type: type, cost: cost };
+	//const params = {weight: weight, type: type, cost: cost };
 
 	// Render the response, using "result.ejs" in the pages directory
-	response.render('pages/result', params);
+	//response.render('pages/result', params);
 }
 
